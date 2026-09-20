6080f970b99cb9ed8549fb0a43ce03f65eaa7b94
Tshepo-Mamakoko <mamakokotshepo44@gmail.com>
2026-09-20 16:41:40 +0200

    moved the group_calculator to the slabs_group-activity2

---
+++ group_calculator.py
@@ -0,0 +1,42 @@
+# Southern Labs Institute of Technology
+# Student: Tshepo Mamakoko
+# Instructor: Idah khumalo
+
+# Wed 16 September 2026
+# KM-02-KT01: Introduction to programming
+
+#KT0101
+#KT0102
+#KT0103
+#KT0104
+
+# Group Activity 2 : Python Syntax and Git Collaboration
+# Start date: 16 September 2026
+# Due date : 16 September 2026
+
+"""
+Tasks
+"""
+
+
+"""
+Task 1_Python Script Development(group_calculator.py) [15 Marks)
+
+"""
+
+def input_and_variables(num):
+
+	"""
+	Prompts the user to enter:
+	two numbers
+	their goup name using
+	input()
+	
+	"""
+	num_1 = int(input("what is the first number? : "))
+	num_2 = int(input("what is the second number? : "))
+	
+	print("these are your numbers: " + str(num_1) + str(num_2))
+
+# input_and_variables(num)
+
